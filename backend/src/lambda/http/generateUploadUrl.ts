46a00b52d9aa204b268bb22208228da68d17a8e2
Andrey Konopkov <konopkov@gmail.com>
2020-04-22 20:44:39 +0300

    fix: Updating attachmentUrl on generateUploadUrl (#5)
    * fix: Updating attachmentUrl on generateUploadUrl request

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -3,17 +3,24 @@ import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as AWS  from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+import { getUploadUrl, getAttachmentUrl } from '../../dataLayer/s3Access'
+import { updateAttachmentUrl } from '../../businessLogic/todos'
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
   const todoId = event.pathParameters.todoId
-  const url = getUploadUrl(todoId)
+  const uploadUrl = getUploadUrl(todoId)
+  const attachmentUrl = getAttachmentUrl(todoId)
+
+  await updateAttachmentUrl(todoId, attachmentUrl, jwtToken)
 
   return {
     statusCode: 201,
     body: JSON.stringify({
-      uploadUrl: url
+      uploadUrl: uploadUrl
     })
   }
 })
@@ -23,19 +30,3 @@ handler.use(
     credentials: true
   })
 )
-
-const XAWS = AWSXRay.captureAWS(AWS)
-const s3 = new XAWS.S3({
-  signatureVersion: 'v4'
-})
-
-const bucketName = process.env.IMAGES_S3_BUCKET
-const urlExpiration = process.env.SIGNED_URL_EXPIRATION
-
-function getUploadUrl(imageId: string) {
-  return s3.getSignedUrl('putObject', {
-    Bucket: bucketName,
-    Key: imageId,
-    Expires: urlExpiration
-  })
-}
